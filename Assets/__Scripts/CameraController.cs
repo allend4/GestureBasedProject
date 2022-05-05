9c558e837d8ae50c47aa6647bf5f304a211e8bcd
DAVID ALLEN <G00375372@gmit.ie>
2022-05-05 17:38:23 +0100

    camera follows bowling ball

---
+++ Assets/__Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get offset
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // follow ball
+        if (ball.transform.position.z < 5)
+            transform.position = ball.transform.position + offset;
+    }
+}
