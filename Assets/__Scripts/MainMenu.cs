0c2de4f1e2ca1c3954b95c1c1967ba7b097476bc
DAVID ALLEN <G00375372@gmit.ie>
2022-05-06 00:36:16 +0100

    myo gestures added to main menu

--- Assets/__Scripts/MainMenu.cs
+++ Assets/__Scripts/MainMenu.cs
@@ -3,8 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using Vibrationtype = Thalmic.Myo.VibrationType;
+
 public class MainMenu : MonoBehaviour
 {
+    public GameObject myo = null;
+    private Pose lastPose = Pose.Unknown;
+
+    private void Update()
+    {
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        if (thalmicMyo.pose != lastPose)
+        {
+            // wave out to start game
+            if (thalmicMyo.pose == Pose.WaveOut)
+            {
+                PlayGame();
+                Debug.Log("Wave Out");
+            }
+            // wave in to quit game
+            else if (thalmicMyo.pose == Pose.WaveIn)
+            {
+                QuitGame();
+                Debug.Log("Wave In");
+            }
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,6 +43,18 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit Application!");
         Application.Quit();
+    }
+
+   void ExtendUnlock (ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
     }
 
 }
